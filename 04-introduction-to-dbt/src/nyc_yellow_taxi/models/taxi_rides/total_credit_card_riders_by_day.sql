53a758966a3d3e8d869440b506f5954de7e0cce4
Jorge Roldan <jorge.roldan@gmail.com>
2024-12-20 18:07:54 +0100

    Hierarchical models in dbt

--- 04-introduction-to-dbt/src/nyc_yellow_taxi/models/taxi_rides/total_credit_card_riders_by_day.sql
+++ 04-introduction-to-dbt/src/nyc_yellow_taxi/models/taxi_rides/total_credit_card_riders_by_day.sql
@@ -3,6 +3,6 @@
 select 
    date_part('day', tpep_pickup_datetime) as day,
    count(*) as total_rides
-from taxi_rides_raw
-where Payment_type = 1
+from {{ ref('taxi_rides_raw') }}
+where payment_type = 1
 group by day

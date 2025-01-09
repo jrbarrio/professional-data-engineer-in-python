39199fdbddb2837794a1eeff3e23718040e4ca31
Jorge Roldan <jorge.roldan@gmail.com>
2025-01-09 18:18:40 +0100

    Class vs. instance attributes

---
+++ 05-introduction-to-object-oriented-programming-in-python/src/2-inheritance-and-polimorfism/1.1-class-level-attributes.py
@@ -0,0 +1,13 @@
+# Create a Player class
+class Player:
+  
+  # Create MAX_POSITION class attribute
+  MAX_POSITION = 10
+  
+  # Add a constructor, setting position to zero
+  def __init__(self):
+    self.position = 0
+
+# Create a player p and print its MAX_POSITION
+p = Player()
+print(p.MAX_POSITION)

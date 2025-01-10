8f70f8cdf74db0b87b886918e28330bd15a10736
Jorge Roldan <jorge.roldan@gmail.com>
2025-01-10 18:55:33 +0100

    Class methods

---
+++ 05-introduction-to-object-oriented-programming-in-python/src/2-inheritance-and-polimorfism/2.1-adding-an-alternative-constructor.py
@@ -0,0 +1,16 @@
+class Person:
+  CURRENT_YEAR = 2024
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  
+  # Add a class method decorator
+  @classmethod
+  # Define the from_birth_year method
+  def from_birth_year(cls, name, birth_year):
+    # Create age
+    age = cls.CURRENT_YEAR - birth_year
+    # Return the name and age
+    return cls(name, age)
+
+bob = Person.from_birth_year("Bob", 1990)

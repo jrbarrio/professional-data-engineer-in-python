ca2899dd7a68d392e78dfc09e9f402e9ad8aa3d3
Jorge Roldan <jorge.roldan@gmail.com>
2025-01-11 12:06:52 +0100

    Operator overloading: comparing objects

---
+++ 05-introduction-to-object-oriented-programming-in-python/src/3-integrating-with-standard-python.py/3.2-checking-class-equality.py
@@ -0,0 +1,24 @@
+class Phone:
+    def __init__(self, number):
+        self.number = number
+
+    def __eq__(self, other):
+        return self.number == \
+               other.number
+
+class BankAccount:
+  def __init__(self, number, balance=0):
+    self.number, self.balance = number, balance
+      
+  def withdraw(self, amount):
+    self.balance -= amount 
+
+  # Modify to add a check for the class type
+  def __eq__(self, other):
+    return (self.number == other.number) and (type(self) == type(other))
+
+acct = BankAccount(873555333)
+pn = Phone(873555333)
+
+# Check if the two objects are equal
+print(acct == pn)

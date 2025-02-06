68236307f0d7c5958b3dc791c0abd999ba9e790a
Jorge Roldan <jorge.roldan@gmail.com>
2025-02-06 19:29:43 +0100

    Manipulating data

---
+++ 11-introduction-to-pyspark/src/2-manipulating-data/2.8-aggregating-ii.py
@@ -0,0 +1,5 @@
+# Average duration of Delta flights
+flights.filter(flights.carrier == "DL").filter(flights.origin == "SEA").groupBy().avg("air_time").show()
+
+# Total hours in the air
+flights.withColumn("duration_hrs", flights.air_time/60).groupBy().sum("duration_hrs").show()

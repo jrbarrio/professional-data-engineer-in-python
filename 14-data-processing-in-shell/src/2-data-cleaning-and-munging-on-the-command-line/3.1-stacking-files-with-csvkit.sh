e704b25bdc1324e80ce4de1131f7f47815f7c5c9
Jorge Roldan <jorge.roldan@gmail.com>
2025-02-20 20:11:54 +0100

    Stacking data and chaining commands with csvkit

---
+++ 14-data-processing-in-shell/src/2-data-cleaning-and-munging-on-the-command-line/3.1-stacking-files-with-csvkit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Stack the two files and save results as a new file
+csvstack SpotifyData_PopularityRank6.csv SpotifyData_PopularityRank7.csv > SpotifyPopularity.csv
+
+# Preview the newly created file 
+csvlook SpotifyPopularity.csv

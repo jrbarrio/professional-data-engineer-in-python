6c81b3dc4afeeb6fc71cabebb412f54e90e57568
Jorge Roldan <jorge.roldan@gmail.com>
2025-02-22 15:12:15 +0100

    Python package installation with pip

---
+++ 14-data-processing-in-shell/src/4-data-pipeline-on-the-command-line/2.2-installing-python-dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Add scikit-learn to the requirements.txt file
+echo "scikit-learn" > requirements.txt
+
+# Preview file content
+cat requirements.txt
+
+# Install the required dependencies
+pip install -r requirements.txt
+
+# Verify that Scikit-Learn is now installed
+pip list
